693466887ea4905bbc388b6c5df9c38e5cf3c4f7
Jim Fan <iamjimfan@gmail.com>
2024-03-23 23:35:03 +0000

    Add PostAdmin for customising view of a post

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from blog.models import Post
 
-# Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    # Fields to display in list view
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+
+    # Fields to display on right-side bar with filter values
+    list_filter = ['status', 'created', 'publish', 'author']
+
+    # Searchable fields
+    search_fields = ['title', 'body']
+
+    # Fancy but not very useful
+    prepopulated_fields = {'slug': ('title',)}
+
+    # Good to have search feature for user, but display raw ID is a strange
+    raw_id_fields = ['author']
+
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
